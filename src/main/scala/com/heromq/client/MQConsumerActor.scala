00196e98768aa07098bfa4df47466a0490edec42
Ivan Mikushin <i.mikushin@gmail.com>
2012-12-04 13:52:29 +0600

    new client interface

--- src/main/scala/com/heromq/client/MQConsumerActor.scala
+++ src/main/scala/com/heromq/client/MQConsumerActor.scala
@@ -1,5 +1,8 @@
 package com.heromq.client
 
-import com.heromq.akka.{ConsumerActor, MQ}
+import com.heromq.akka.{ProducerActor, PubSub, ConsumerActor, MQ}
 
 class MQConsumerActor(val brokerUri: String, val destinationName: String) extends ConsumerActor with MQ
+class PubSubConsumerActor(val brokerUri: String, val destinationName: String) extends ConsumerActor with PubSub
+class MQProducerActor(val brokerUri: String, val destinationName: String) extends ProducerActor with MQ
+class PubSubProducerActor(val brokerUri: String, val destinationName: String) extends ProducerActor with PubSub
